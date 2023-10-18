6f25bca95e432979aa216c959fbd636ec3aa75f3
root <marektobias@email.cz>
2023-10-18 11:49:27 +0200

    Added assertion tests

--- tests/sheets/test_population_selector.py
+++ tests/sheets/test_population_selector.py
@@ -1,5 +1,6 @@
 import mozaik
 from mozaik.core import ParametrizedObject
+from parameters import ParameterSet
 from mozaik.tools.distribution_parametrization import MozaikExtendedParameterSet
 
 import pytest
@@ -26,27 +27,25 @@ from mozaik.sheets.population_selector import PopulationSelector
 
 class TestPopulationSelector:
 
-    @pytest.fixture(scope="class", params=["retinal_sheet"]) # muzu z SheetsTests asi brát
-    def params(self, request):
-        """A fixture for getting the current population selector's parameters"""
-        yield MozaikExtendedParameterSet(f"tests/sheets/SheetsTests/param/{request.param}")
-
-
     @pytest.fixture(scope="class")
-    def init_pop_selector(self, mock_sheet, params):
+    def init_pop_selector(self, mock_sheet):
         """A fixture for the initialization of the PopulationSelector object, created once"""
+        params = ParameterSet({})
+
         yield PopulationSelector(mock_sheet, params), params
 
 
     # __INIT__
 
-    def test_init_assertions(self, init_pop_selector):
-        pop_sel, params = init_pop_selector
+    def test_init_assertions(self, init_pop_selector, mock_sheet):
+        pop_sel, _ = init_pop_selector
 
-        # assert pop_sel.sheet == params.sheet # nebo teda mock_sheet
+        assert pop_sel.sheet == mock_sheet
 
 
-    def test_init_parametrized_object_call(self, mock_sheet, params):
+    def test_init_parametrized_object_call(self, mock_sheet):
+        params = ParameterSet({})
+
         with patch.object(ParametrizedObject, '__init__') as mock_parent_call:
             pop_sel = PopulationSelector(mock_sheet, params)
 
@@ -64,13 +63,21 @@ class TestPopulationSelector:
 
 
 
+from mozaik.sheets.population_selector import RCAll
 
 class TestRCAll:
 
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet):
+        params = ParameterSet({})
+
+        yield RCAll(mock_sheet, params), params
+
+
     # GENERATE_IDD_LIST_OF_NEURONS
 
-    def test_generate_idd_list_of_neurons(self):#, init_pop_selector):
-        # pop_sel, _ = init_pop_selector
+    def test_generate_idd_list_of_neurons(self, init_pop_selector):
+        pop_sel, _ = init_pop_selector
         ...
         # selected_pop = pop_sel.generate_idd_list_of_neurons
 
@@ -80,53 +87,91 @@ class TestRCAll:
 
 
 
+from mozaik.sheets.population_selector import RCRandomN
+
 class TestRCRandomN:
+
+    @pytest.fixture(scope="class", params=["rc_random_n_pop_sel"])
+    def params(self, request):
+        """A fixture for getting the current population selector's parameters"""
+        yield MozaikExtendedParameterSet(f"tests/sheets/PopulationSelectorTests/param/{request.param}")
+    
+    
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet, params):
+        """A fixture for the initialization of the PopulationSelector object, created once"""
+        yield RCRandomN(mock_sheet, params), params
+
 
     # __INIT__
 
-    def test_init_assertions(self):#, init_pop_selector):
-        # pop_sel, params = init_pop_selector
-        ...
-        # assert params... == pop_sel.num_of_cells # int
+    def test_init_assertions(self, init_pop_selector):
+        pop_sel, params = init_pop_selector
+        
+        assert params.num_of_cells == pop_sel.parameters.num_of_cells # int
 
 
     # GENERATE_IDD_LIST_OF_NEURONS
 
-    def test_generate_idd_list_of_neurons(self):
+    def test_generate_idd_list_of_neurons(self): # what if n > than real number of cells
         ...
 
 
 
+
+from mozaik.sheets.population_selector import RCRandomPercentage
 
 class TestRCRandomPercentage:
 
+    @pytest.fixture(scope="class", params=["rc_random_percentage_pop_sel"])
+    def params(self, request):
+        yield MozaikExtendedParameterSet(f"tests/sheets/PopulationSelectorTests/param/{request.param}")
+    
+    
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet, params):
+        yield RCRandomPercentage(mock_sheet, params), params
+
+
     # __INIT__
 
-    def test_init_assertions(self):#, init_pop_selector):
-        # pop_sel, params = init_pop_selector
-        ...
-        # assert params... == pop_sel.percentage # float
+    def test_init_assertions(self, init_pop_selector):
+        pop_sel, params = init_pop_selector
+        
+        assert params.percentage == pop_sel.parameters.percentage # float
 
 
     # GENERATE_IDD_LIST_OF_NEURONS
 
-    def test_generate_idd_list_of_neurons(self):
+    def test_generate_idd_list_of_neurons(self): # what if percentage > 1.0
         ...
 
 
 
+
+from mozaik.sheets.population_selector import RCGrid
 
 class TestRCGrid:
 
+    @pytest.fixture(scope="class", params=["rc_grid_pop_sel"])
+    def params(self, request):
+        yield MozaikExtendedParameterSet(f"tests/sheets/PopulationSelectorTests/param/{request.param}")
+    
+    
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet, params):
+        yield RCGrid(mock_sheet, params), params
+
+
     # __INIT__
 
-    def test_init_assertions(self):#, init_pop_selector):
-        # pop_sel, params = init_pop_selector
-        ...
-        # assert params... == pop_sel.size # float
-        # assert params... == pop_sel.spacing  # float
-        # assert params... == pop_sel.offset_x  # float
-        # assert params... == pop_sel.offset_y # float
+    def test_init_assertions(self, init_pop_selector):
+        pop_sel, params = init_pop_selector
+        
+        assert params.size == pop_sel.parameters.size # float
+        assert params.spacing == pop_sel.parameters.spacing  # float
+        assert params.offset_x == pop_sel.parameters.offset_x  # float
+        assert params.offset_y == pop_sel.parameters.offset_y # float
 
 
     # GENERATE_IDD_LIST_OF_NEURONS
@@ -136,18 +181,30 @@ class TestRCGrid:
 
 
 
+
+from mozaik.sheets.population_selector import RCGridDegree
 
 class TestRCGridDegree:
 
+    @pytest.fixture(scope="class", params=["rc_grid_degree_pop_sel"])
+    def params(self, request):
+        yield MozaikExtendedParameterSet(f"tests/sheets/PopulationSelectorTests/param/{request.param}")
+    
+    
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet, params):
+        yield RCGridDegree(mock_sheet, params), params
+
+
     # __INIT__
 
-    def test_init_assertions(self):#, init_pop_selector):
-        # pop_sel, params = init_pop_selector
-        ...
-        # assert params... == pop_sel.size  # float
-        # assert params... == pop_sel.spacing  # float
-        # assert params... == pop_sel.offset_x  # float
-        # assert params... == pop_sel.offset_y  # float
+    def test_init_assertions(self, init_pop_selector):
+        pop_sel, params = init_pop_selector
+        
+        assert params.size == pop_sel.parameters.size # float
+        assert params.spacing == pop_sel.parameters.spacing  # float
+        assert params.offset_x == pop_sel.parameters.offset_x  # float
+        assert params.offset_y == pop_sel.parameters.offset_y # float
 
 
     # GENERATE_IDD_LIST_OF_NEURONS
@@ -158,18 +215,30 @@ class TestRCGridDegree:
 
 
 
+from mozaik.sheets.population_selector import SimilarAnnotationSelector
+
 class TestSimilarAnnotationSelector:
+
+    @pytest.fixture(scope="class", params=["similar_annotation_selector"])
+    def params(self, request):
+        yield MozaikExtendedParameterSet(f"tests/sheets/PopulationSelectorTests/param/{request.param}")
+    
+    
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet, params):
+        yield SimilarAnnotationSelector(mock_sheet, params), params
+
 
     # __INIT__
 
-    def test_init_assertions(self):#, init_pop_selector):
-        # pop_sel, params = init_pop_selector
-        ...
-        # assert params... == pop_sel.annotation  # str
-        # assert params... == pop_sel.distance  # float
-        # assert params... == pop_sel.value  # float
-        # assert params... == pop_sel.num_of_cells  # int
-        # assert params... == pop_sel.period  # float
+    def test_init_assertions(self, init_pop_selector):
+        pop_sel, params = init_pop_selector
+        
+        assert params.annotation == pop_sel.parameters.annotation  # str
+        assert params.distance == pop_sel.parameters.distance  # float
+        assert params.value == pop_sel.parameters.value  # float
+        assert params.num_of_cells == pop_sel.parameters.num_of_cells  # int
+        assert params.period == pop_sel.parameters.period  # float
 
 
     # PICK_CLOSE_TO_ANNOTATION
@@ -186,16 +255,34 @@ class TestSimilarAnnotationSelector:
 
 
 
+from mozaik.sheets.population_selector import SimilarAnnotationSelectorRegion
+
 class TestSimilarAnnotationSelectorRegion:
+
+    @pytest.fixture(scope="class", params=["similar_annotation_selector_region"])
+    def params(self, request):
+        yield MozaikExtendedParameterSet(f"tests/sheets/PopulationSelectorTests/param/{request.param}")
+    
+    
+    @pytest.fixture(scope="class")
+    def init_pop_selector(self, mock_sheet, params):
+        yield SimilarAnnotationSelectorRegion(mock_sheet, params), params
+
 
     # __INIT__
 
-    def test_init_assertions(self):#, init_pop_selector):
-        # pop_sel, params = init_pop_selector
-        ...
-        # assert params... == pop_sel.size  # float
-        # assert params... == pop_sel.offset_x  # float
-        # assert params... == pop_sel.offset_y  # float
+    def test_init_assertions(self, init_pop_selector):
+        pop_sel, params = init_pop_selector
+        
+        assert params.annotation == pop_sel.parameters.annotation  # str
+        assert params.distance == pop_sel.parameters.distance  # float
+        assert params.value == pop_sel.parameters.value  # float
+        assert params.num_of_cells == pop_sel.parameters.num_of_cells  # int
+        assert params.period == pop_sel.parameters.period  # float
+
+        assert params.size == pop_sel.parameters.size # float
+        assert params.offset_x == pop_sel.parameters.offset_x  # float
+        assert params.offset_y == pop_sel.parameters.offset_y # float
 
 
     # GENERATE_IDD_LIST_OF_NEURONS
